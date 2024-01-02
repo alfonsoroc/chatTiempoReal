696fab8e5ab986b7db4001d33c9ccfefba7aeaaf
alfonsoroc <alfonsoroc789@gmail.com>
2024-01-02 16:43:29 -0700

    Comunicacion servidor cliente

--- server/index.js
+++ server/index.js
@@ -16,6 +16,11 @@ io.on('connection', (socket)=>{
     socket.on('disconnect',()=>{
         console.log('an user has disconnected')
     })
+
+    socket.on('chat message',(msg)=>{
+        io.emit('chat message',msg)
+    })
+
 })
 
 
