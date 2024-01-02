43cf2ae325d0c9bcbbfd5987c1789b20bed3a6ad
alfonsoroc <alfonsoroc789@gmail.com>
2024-01-02 15:18:33 -0700

    Cliente-servidor

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 import express from 'express'
 import logger from 'morgan'
 
+import {Server} from 'socket.io'
+import {createServer} from 'node:http'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', (socket)=>{
+    console.log('a user has connected!')
+
+    socket.on('disconnect',()=>{
+        console.log('an user has disconnected')
+    })
+})
+
+
 app.use(logger('dev'))
 
 app.get('/',(req,res)=>{
@@ -11,7 +26,7 @@ app.get('/',(req,res)=>{
 
 })
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log('server running om port' + port)
 })
 
