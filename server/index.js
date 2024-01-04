4fe9966dc3e438ae46e4f3d3a0fca4e1ed19040f
alfonsoroc <alfonsoroc789@gmail.com>
2024-01-04 09:10:06 -0700

    update

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import logger from 'morgan'
 
-import {Server} from 'socket.io'
-import {createServer} from 'node:http'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 import dotenv from 'dotenv'
 import { createClient } from '@libsql/client'
@@ -13,13 +13,13 @@ const port = process.env.PORT ?? 3000
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server,{
-    connectionStateRecovery:{}
+const io = new Server(server, {
+    connectionStateRecovery: {}
 })
 
 const db = createClient({
-    url:"libsql://present-newton-destine-alfonsoroc.turso.io",
-    authToken:process.env.DB_TOKEN
+    url: "libsql://present-newton-destine-alfonsoroc.turso.io",
+    authToken: process.env.DB_TOKEN
 })
 
 await db.execute(`
@@ -31,19 +31,19 @@ await db.execute(`
 
 
 
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
     console.log('a user has connected!')
 
-    socket.on('disconnect',()=>{
+    socket.on('disconnect', () => {
         console.log('an user has disconnected')
     })
 
-    socket.on('chat message', async (msg)=>{
+    socket.on('chat message', async (msg) => {
         let result
         try {
             result = await db.execute({
-                sql:'INSERT INTO messages (content) VALUES (:msg)',
-                args:{ msg }
+                sql: 'INSERT INTO messages (content) VALUES (:msg)',
+                args: { msg }
             })
         } catch (error) {
             console.error(error)
@@ -51,22 +51,22 @@ io.on('connection', (socket)=>{
         }
 
 
-        io.emit('chat message',msg,result.lastInsertRowid.toString())
+        io.emit('chat message', msg, result.lastInsertRowid.toString())
     })
 
-    
+
 
 })
 
 
 app.use(logger('dev'))
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
 
 })
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log('server running om port' + port)
 })
 
